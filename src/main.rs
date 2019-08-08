ba7e76186ee9205a2092ce37d673d2a66961261e
Hamid Ghadyani <hamid@hamid.cc>
2019-08-07 22:57:57 -0400

    Proper location for socket file.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use dirs;
 use serde::Deserialize;
 use socket2::SockAddr;
 use socket2::{Domain, Socket, Type};
-use std::fs::remove_file;
+use std::fs::{create_dir_all, remove_file};
 use std::io::Error;
 use std::thread;
 use std::time::Duration;
@@ -9,7 +10,7 @@ use uuid::Uuid;
 
 use serde_derive::{Deserialize, Serialize};
 
-const SRV_SOCKET_FN: &str = "/Users/hamid/tmp/srv.socket";
+const SRV_SOCKET_FN: &str = ".giv.sock";
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Item {
@@ -31,9 +32,13 @@ enum Input {
 }
 
 fn main() {
-    let _ = remove_file(SRV_SOCKET_FN);
+    let mut socket_fn = dirs::data_dir().unwrap();
+    socket_fn.push("giv");
+    create_dir_all(socket_fn.as_path()).unwrap();
+    socket_fn.push(SRV_SOCKET_FN);
+    let _ = remove_file(&socket_fn);
     let socket_srv = Socket::new(Domain::unix(), Type::stream(), None).unwrap();
-    let addr_srv = SockAddr::unix(SRV_SOCKET_FN).unwrap();
+    let addr_srv = SockAddr::unix(&socket_fn).unwrap();
 
     match socket_srv.bind(&addr_srv) {
         Ok(()) => println!(" bind success to server socket"),
@@ -45,7 +50,7 @@ fn main() {
     socket_srv.listen(128).unwrap();
 
     loop {
-        let (sock, addr) = socket_srv.accept().unwrap();
+        let (sock, _) = socket_srv.accept().unwrap();
         println!("connection from {:?}", sock);
         let mut de = serde_json::Deserializer::from_reader(&sock);
         let item = Item::deserialize(&mut de).unwrap();
