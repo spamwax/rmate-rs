6478533c0d315fc8aba45537eee6089b7fbcfb25
Hamid Ghadyani <hamid@hamid.cc>
2020-03-09 04:50:52 -0400

    Trying to do shit.

--- src/settings.rs
+++ src/settings.rs
@@ -1,4 +1,5 @@
 use std::ffi::OsString;
+use std::path::PathBuf;
 
 pub struct Settings {
     pub host: String,
@@ -9,6 +10,17 @@ pub struct Settings {
     pub names: Vec<OsString>,
     pub files: Vec<OsString>,
 }
+
+// use std::collections::hash_map::HashMap;
+
+pub(crate) struct OpenedBuffer {
+    pub(crate) path: PathBuf,
+    pub(crate) name: OsString,
+    pub(crate) canwrite: bool,
+    pub(crate) size: usize,
+}
+
+// type MyItems<String, OpenedBuffers> = HashMap<String, OpenedBuffers>;
 
 impl Settings {
     pub fn new(port: u16) -> Self {
