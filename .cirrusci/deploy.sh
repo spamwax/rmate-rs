3f51f0e3742bf59b57189b80cfc17863f06ee6eb
Hamid Ghadyani <hamid@hamid.cc>
2020-03-29 17:21:32 -0400

    ghr_exe path issue

--- .cirrusci/deploy.sh
+++ .cirrusci/deploy.sh
@@ -7,17 +7,20 @@ set -ex
 export PATH=$HOME/.cargo/bin:$PATH
 
 main() {
-    export ghr_exe=$HOME/ghr/ghr
     local src=$(pwd) stage=$(mktemp -d)
+    export ghr_exe="$src/ghr/ghr"
+
     os_name="$CIRRUS_OS"-$(freebsd-version -u | cut -f 1 -d '-') || os_name=freebsd-12.1
     arch=x86_$(getconf LONG_BIT) || arch=x86_64
-    CIRRUS_SHA1=$(git rev-parse --verify HEAD) || true
-    echo $CIRRUS_CHANGE_IN_REPO
+
     export artifacts=rmate_"$os_name"_"$arch.tar.gz"
     tar czvf "$artifacts" "target/release/rmate" || true
     ls -lh
     [ -f "$artifacts" ] || true
+
+    CIRRUS_SHA1=$CIRRUS_CHANGE_IN_REPO
     echo ${CIRRUS_REPO_OWNER} ${CIRRUS_REPO_NAME} ${CIRRUS_SHA1} ${CIRRUS_TAG} ${artifacts}
+
     [ -f "$ghr_exe" ] && ls -lh "$ghr_exe"
     "$ghr_exe" -t ${GITHUB_TOKEN} -u ${CIRRUS_REPO_OWNER} -r ${CIRRUS_REPO_NAME} -c ${CIRRUS_SHA1} -delete ${CIRRUS_TAG} ${artifacts} || true
 }
