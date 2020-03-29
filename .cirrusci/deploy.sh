f20ae699ce22f63e99712ca6d0e4af9be0487b23
Hamid Ghadyani <hamid@hamid.cc>
2020-03-29 04:21:29 -0400

    Rename CIRCLE to CIRRUS

--- .cirrusci/deploy.sh
+++ .cirrusci/deploy.sh
@@ -14,14 +14,15 @@ main() {
     CIRRUS_SHA1=$(git rev-parse --verify HEAD) || true
     export artifacts=rmate_"$os_name"_"$arch.tar.gz"
     tar czvf "$artifacts" "target/release/rmate" || true
-    ls -l
-    echo ${CIRRUS_REPO_OWNER} ${CIRRUS_REPO_NAME} ${CIRRUS_SHA1} ${CIRCLE_TAG} ${artifacts}
-    [ -f "$ghr" ] && ls -lh "$ghr"
-    "$ghr_exe" -t ${GITHUB_TOKEN} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -c ${CIRRUS_SHA1} -delete ${CIRRUS_TAG} ${artifacts} || true
+    ls -lh
+    [ -f "$artifacts" ] || true
+    echo ${CIRRUS_REPO_OWNER} ${CIRRUS_REPO_NAME} ${CIRRUS_SHA1} ${CIRRUS_TAG} ${artifacts}
+    [ -f "$ghr_exe" ] && ls -lh "$ghr_exe"
+    "$ghr_exe" -t ${GITHUB_TOKEN} -u ${CIRRUS_REPO_OWNER} -r ${CIRRUS_REPO_NAME} -c ${CIRRUS_SHA1} -delete ${CIRRUS_TAG} ${artifacts} || true
 }
 
 if [ -n "$CIRRUS_TEST" ]; then
-    echo "CIRCLE_TEST is set, exitting"
+    echo "CIRRUS_TEST is set, exitting"
     exit 1 || true
 fi
 if [ -z "$CIRRUS_TAG" ]; then
