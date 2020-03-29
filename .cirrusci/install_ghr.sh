e90ce026969eacc3d7685342ef067250943a26e0
Hamid Ghadyani <hamid@hamid.cc>
2020-03-29 17:30:29 -0400

    caching not working.

--- .cirrusci/install_ghr.sh
+++ .cirrusci/install_ghr.sh
@@ -9,5 +9,7 @@ pwd
 # /usr/sbin/pkg install -y git
 
 # cd /tmp && git clone https://github.com/tcnksm/ghr
+
+[ -f `pwd`/ghr/ghr ] || ls -lh .
 git clone https://github.com/tcnksm/ghr
 cd ghr && go build || true
