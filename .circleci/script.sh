f1a4b105b5ad8c561050a3766813147d10fb92af
Hamid Ghadyani <hamid@hamid.cc>
2020-04-17 20:02:40 -0400

    don't error out

--- .circleci/script.sh
+++ .circleci/script.sh
@@ -10,31 +10,31 @@ run_tests() {
     export RMATE_PORT=55555
     case "$TARGET" in
         x86_64-apple-darwin)
-            $runner -vvv -w Cargo.toml 2>output.log
+            $runner -vvv -w Cargo.toml 2>output.log || echo
             ;;
         x86_64-unknown-linux-gnu)
-            $runner -vvv -w Cargo.toml 2>output.log
+            $runner -vvv -w Cargo.toml 2>output.log || echo
             ;;
         i686-unknown-linux-gnu)
-            $runner -vvv -w Cargo.toml 2>output.log
+            $runner -vvv -w Cargo.toml 2>output.log || echo
             ;;
         i686-apple-darwin)
-            $runner -vvv -w Cargo.toml 2>output.log
+            $runner -vvv -w Cargo.toml 2>output.log || echo
             ;;
         x86_64-unknown-freebsd)
-            $runner -vvv -w Cargo.toml 2>output.log
+            $runner -vvv -w Cargo.toml 2>output.log || echo
             # $runner config --authorization hamid:12345
             # $runner config -d
             ;;
         armv7-linux-androideabi)
-            $runner -vvv -w Cargo.toml 2>output.log
+            $runner -vvv -w Cargo.toml 2>output.log || echo
             ;;
         *)
             return
             ;;
     esac
     grep "Error: \"Connection refused (os error" ./output.log
-    echo && cat output.log
+    echo && cat output.log || echo
 }
 
 # if [[ "$TARGET" == "i686-unknown-linux-gnu" ]]; then
