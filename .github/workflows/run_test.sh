be5191636f09c60e8300b972496ac62e95b25b4d
Hamid Ghadyani <hamid@hamid.cc>
2022-07-18 06:04:18 -0400

    Add building binaries for Android.

--- .github/workflows/run_test.sh
+++ .github/workflows/run_test.sh
@@ -19,14 +19,17 @@ echo "Running tests using target/${GREEN}$TARGET${NC}/debug/rmate"; echo
 binary_path=target/"$TARGET/$BUILD_TYPE"/rmate
 file "$binary_path"
 
+# use cross if building on special platforms
 if [[ -n "$USE_CROSS" && "$USE_CROSS" == "true" ]]; then
     echo "${GREEN}$TARGET${NC} binary ${RED}cannot be executed${NC} on Linux. Use Rust's cross."
     # Show help message
     cross run --target "$TARGET" -- --help || echo
     # Test with local .rmate.rc
     cross run --target "$TARGET" -- -vvv -w Cargo.toml 2>output.log || echo
-    grep "Connection refused (os error " ./output.log
-    cat output.log
+    if ! grep -q "Connection refused (os error " ./output.log; then
+        cat ./output.log
+        exit 1
+    fi
     printf "\n\n\n"
     sleep 2
     exit 0
@@ -49,13 +52,25 @@ if [[ -z "$ARM" || "$ARM" == 'false' ]]; then
     export RMATE_HOST=auto
     export RMATE_PORT=55555
     $binary_path -vvv -w Cargo.toml 2>output.log || echo
-    grep "Connection refused (os error " ./output.log
-    pcregrep -M 'host: Some\(\n\s+"localhost",\n\s+\),\n\s+port: Some\(\n\s+55555,\n\s+\),' ./output.log
-    grep "Finding host automatically from SSH_CONNECTION" ./output.log
-    grep "from SSH_CONNECTION: localhost" ./output.log
+    if ! grep -q "Connection refused (os error " ./output.log; then
+        cat ./output.log
+        exit 1
+    fi
+    if pcregrep -q -M 'host: Some\(\n\s+"localhost",\n\s+\),\n\s+port: Some\(\n\s+55555,\n\s+\),' ./output.log; then
+        cat ./output.log
+        exit 1
+    fi
+    if ! grep -q "Finding host automatically from SSH_CONNECTION" ./output.log; then
+        cat ./output.log
+        exit 1
+    fi
+    if ! grep -q "from SSH_CONNECTION: localhost" ./output.log; then
+        cat ./output.log
+        exit 1
+    fi
     printf "\n\n\n"
     sleep 2
-else
+else # Use qemu to run ARM-based binaries for Linux OS.
     if [[ "$TARGET" == "aarch64-unknown-linux-gnu" ]]; then
         libpath="/usr/aarch64-linux-gnu"
         export LD_LIBRARY_PATH=$libpath/lib64
@@ -65,22 +80,16 @@ else
         export LD_LIBRARY_PATH=$libpath/lib
         arm_runner="qemu-arm-static"
     fi
-    ls -l "$libpath"
-    ls -l "$libpath/lib64" || true
-    ls -l "$libpath/lib" || true
-    echo $(which $arm_runner)
-    # $arm_runner --help || true
-    # $arm_runner -v || true
     $arm_runner -L $libpath "$binary_path" -vvv -w Cargo.toml 2>output.log
-    echo "exit code: $?\n"
-    # cat output.log || true
-    sleep 2
-    grep "Connection refused (os error " ./output.log
-    echo "exit code: $?\n"
-    grep "Read disk settings-> { host: Some(" ./output.log
-    echo "exit code: $?\n"
+    if ! grep -q "Connection refused (os error " ./output.log; then
+        cat ./output.log
+        exit 1
+    fi
+    if grep -q "Read disk settings-> { host: Some(" ./output.log; then
+        cat ./output.log
+        exit 1
+    fi
     printf "\n\n\n"
     sleep 2
 fi
 
-cat ./output.log
